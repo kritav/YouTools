5476ec2d451018d8bc3025d8ebf213895840fc70
kritav <kritav.dalal@gmail.com>
2025-07-07 20:33:59 +0530

    Create background.js

---
+++ background.js
@@ -0,0 +1,29 @@
+const YOUTUBE_ICON = "youtools.png";
+const GRAYSCALE_ICON = "youtools-gray.png";
+
+// Check if current URL is YouTube
+function isYouTube(url) {
+  return url && url.includes("youtube.com");
+}
+
+// Update the extension icon
+function updateIcon(tabId, url) {
+  const iconPath = isYouTube(url) ? YOUTUBE_ICON : GRAYSCALE_ICON;
+  chrome.action.setIcon({ tabId, path: iconPath });
+}
+
+// Tab update handler
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.status === "complete" && tab.url) {
+    updateIcon(tabId, tab.url);
+  }
+});
+
+// Tab switching handler
+chrome.tabs.onActivated.addListener(activeInfo => {
+  chrome.tabs.get(activeInfo.tabId, tab => {
+    if (tab.url) {
+      updateIcon(activeInfo.tabId, tab.url);
+    }
+  });
+});
